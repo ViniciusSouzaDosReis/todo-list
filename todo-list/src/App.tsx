f4a60c535fbbf14fe7d670560fcc86834aa00d53
Vinicius Reis <vinireis010305@gmail.com>
2023-07-03 20:38:37 -0300

    Adicionando Form

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,9 +1,45 @@
-import './App.css'
+import { PlusCircle } from 'phosphor-react'
+
+import styles from './App.module.css'
+
+import './global.css'
+
+import { Header } from './components/Header'
+import { Input } from './components/Input'
+import { Button } from './components/Button'
+
 
 function App() {
   return (
     <>
-      <h1>Ola Mundo!</h1>
+      <Header />
+      <main>
+        <form action="" className={styles.formAddTask}>
+          <Input placeholder='Adicione uma nova tarefa' />
+          <Button>
+            Criar
+            <PlusCircle size={24} color='#F2F2F2' ></PlusCircle>
+          </Button>
+        </form>
+        <section className={styles.taskContainer}>
+          <span className={styles.taskContainerTitleWrapper}>
+            <span>
+              <strong>Tarefas criadas</strong>
+              <span className={styles.counter}>
+                0
+              </span>
+            </span>
+            <span>
+              <strong>
+                Concluídas
+              </strong>
+              <span className={styles.counter}>
+                0
+              </span>
+            </span>
+          </span>
+        </section>
+      </main>
     </>
   )
 }
